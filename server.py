a7f2e285b02f406f33b42e12ce106ceb4784e131
Jonathon <jonathonc.94@gmail.com>
2016-01-17 10:23:09 -0500

    Fixed "Too many retries" bug by adding a try-except method and putting a sleep timer of 1 second in.

--- server.py
+++ server.py
@@ -2,34 +2,37 @@ import json
 import time
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
+from requests import exceptions
 
 from models import *
 from ParseHandler import handler
-
 
 watchFolder = ""
 parseAppId = ""
 parseRESTKey = ""
 
+
 class MyHandler(PatternMatchingEventHandler):
     patterns = ["*.mp3"]
 
-
     def on_created(self, event):
         print "New Call Found"
-        filepath = event.src_path.encode('ascii','ignore')
+        filepath = event.src_path.encode('ascii', 'ignore')
         x = Call(open(filepath))
-        handle = handler(x,parseAppId,parseRESTKey)
-        print "Sending to Parse.com"
-        handle.send()
-
-
-
-
+        try:
+            handle = handler(x, parseAppId, parseRESTKey)
+            print "Sending to Parse.com"
+            handle.send()
+        except exceptions.ConnectionError:
+            print "Sending Failed: To Many Retries. Trying again after 1 second"
+            time.sleep(1)
+            handle = handler(x, parseAppId, parseRESTKey)
+            print "Sending to Parse.com"
+            handle.send()
 
 
 try:
-    with open("server.conf","r") as theConfig:
+    with open("server.conf", "r") as theConfig:
         x = json.loads(theConfig.read())
         watchFolder = x["folder"]
         parseAppId = x["X-Parse-Application-Id"]
@@ -39,10 +42,9 @@ try:
 except IOError:
     print "Error Loading Config File"
 
-
 if __name__ == '__main__':
     observer = Observer()
-    observer.schedule(MyHandler(),path=watchFolder)
+    observer.schedule(MyHandler(), path=watchFolder)
     observer.start()
 
     try:
